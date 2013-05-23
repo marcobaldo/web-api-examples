e9a9804892fb78cd12fa8e4e9a1e834a2d879221
Marc Obaldo <marcklaser@gmail.com>
2013-05-23 16:43:39 +0800

    Updated AngularJS, initial AuthCtrl

--- spa/app/app.js
+++ spa/app/app.js
@@ -1,5 +1,9 @@
-﻿angular.module('spa', ['spa.Vote']).config(function ($routeProvider, $locationProvider) {
+﻿angular.module('spa', ['spa.Vote', 'spa.Auth']).config(function ($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(false);
+    $routeProvider.when('/login', {
+        templateUrl: 'app/login.tpl.html',
+        controller: 'AuthCtrl'
+    });
     $routeProvider.when('/vote', {
         templateUrl: 'app/vote/index.tpl.html',
         controller: 'VoteCtrl'
@@ -7,9 +11,24 @@
     $routeProvider.otherwise({
         redirectTo: '/vote'
     });
+}).run(function ($rootScope, $location, User) {
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        if (!User.IsAuthenticated && next.templateUrl != 'app/login.tpl.html') {
+            $location.path('/login');
+        }
+    });
 });
 
-angular.module('spa.Auth', []).factory('User', function ($http) {
+angular.module('spa.Auth', ['spa.Auth.Controllers', 'spa.Auth.Services']);
+angular.module('spa.Auth.Controllers', []).controller('AuthCtrl', function ($scope, User) {
+    // Sign in
+    var credentials = $scope.credentials = {};
+    $scope.signin = function () {
+        alert('Signing in as ' + credentials.Username + '...');
+    };
+});
+
+angular.module('spa.Auth.Services', []).factory('User', function ($http) {
     return {
         IsAuthenticated: false,
         Principal: null,
